44e8a29dfd07e306f0ffb4e5513d2a64ac78f90e
Juan Gutierrez <juan.gutierrez0157@gmail.com>
2024-02-10 23:02:55 -0500

    Decrease threads from 4 to 2

--- robocopy.ps1
+++ robocopy.ps1
@@ -2,4 +2,4 @@ $source = "ADD_SOURCE_DIRECTORY_HERE"
 $destination = "ADD_DESTINATION_DIRECTORY_HERE"
 $log = "robocopy_$(Get-Date -Format "yyyy_MMdd_HHmm").log"
 
-robocopy $source $destination /log+:$log /tee /mir /z /j /mt:4
+robocopy $source $destination /log+:$log /tee /mir /z /j /mt:2
