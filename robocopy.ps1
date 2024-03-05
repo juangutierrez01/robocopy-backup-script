cee6b205f8ab8068467343a0e7247faf20837f32
Juan Gutierrez <juan.gutierrez0157@gmail.com>
2024-03-05 17:21:46 -0500

    Add LogDirectory to settings

--- robocopy.ps1
+++ robocopy.ps1
@@ -1,14 +1,15 @@
-$LogFile = "robocopy_$(Get-Date -Format "yyyy_MMdd_HHmm").log"
+$Settings = (ConvertFrom-Json (Get-Content -Raw "./settings.json"))
+$LogFile = "$($Settings.LogDirectory)\robocopy_$(Get-Date -Format "yyyy_MMdd_HHmm").log" 
 
-foreach ($Setting in (ConvertFrom-Json (Get-Content -Raw "./settings.json")))
+foreach ($Backup in $Settings.Backups)
 {
     try
     {
-	robocopy "$($Setting.Source)" "$($Setting.Destination)" /log+:"$LogFile" /tee /mir /z /j /mt:1
+	robocopy "$($Backup.Source)" "$($Backup.Destination)" /log+:"$LogFile" /tee /mir /z /j /mt:1
     }
     catch
     {
-	Write-Host $('An error occured while trying to backup "{0}":' -f $Setting.Name)
+	Write-Host $('An error occured while trying to backup "{0}":' -f $Backup.Name)
 	Write-Host $_
     }
 }
