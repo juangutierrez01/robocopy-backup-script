f4c1d6b77dc6175f67bed0ab383f19f9ff8621fe
Juan Gutierrez <juan.gutierrez0157@gmail.com>
2024-03-05 16:46:48 -0500

    Add ability to backup multiple dirs with settings file

--- robocopy.ps1
+++ robocopy.ps1
@@ -1,5 +1,14 @@
-$source = "ADD_SOURCE_DIRECTORY_HERE"
-$destination = "ADD_DESTINATION_DIRECTORY_HERE"
-$logfile = "robocopy_$(Get-Date -Format "yyyy_MMdd_HHmm").log"
+$LogFile = "robocopy_$(Get-Date -Format "yyyy_MMdd_HHmm").log"
 
-robocopy $source $destination /log+:$logfile /tee /mir /z /j /mt:2
+foreach ($Setting in (ConvertFrom-Json (Get-Content -Raw "./settings.json")))
+{
+    try
+    {
+	robocopy "$($Setting.Source)" "$($Setting.Destination)" /log+:"$LogFile" /tee /mir /z /j /mt:1
+    }
+    catch
+    {
+	Write-Host $('An error occured while trying to backup "{0}":' -f $Setting.Name)
+	Write-Host $_
+    }
+}
